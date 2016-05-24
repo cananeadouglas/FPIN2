5eb12b6618e8554aac7ab95eea461e0bac9ba590
Rochester Rocha Gondim <rochagondim@hotmail.com>
2016-05-24 14:29:24 -0300

    Add files via upload

---
+++ criareventos.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+	<title>Cadastro Evento</title>
+	<META http-equiv="Content-Type" content="text/html, charset=UTF-8"/ >
+	<link rel="stylesheet" type="text/css" href="css/css.css"/>
+	<link rel="shortcut icon" href="img/favicon.ico">
+
+	<META name="ifal" content="Site finculado ao Instituto Federal de Alagoas"/>
+	<META name="keywords" content="Eventos em Maceió"/>
+	<META name="author" content="estudantes SI 3° Período"/>
+</head>
+<body>
+	<div id="superior">
+		<center><img src="img/ifal.png"; ></center>
+	</div>
+	<div id="wrap">
+
+		<header>
+			<h3>Criar Evento</h3>
+		</header>
+		<div id="content">
+
+		<form method="post"/></form>
+		<!-- FORMULARIO DE CRIAÇÃO DE EVENTOS -->
+
+      <p><label for="">Nome do Evento</label></br>
+      <input type="text" name="nome" id="nome"></p>
+
+      <p><label for="">Local</label></p></br>
+      <input type="text" name="nome" id="local"><p>
+
+      <p><label for="" >Layout Evento Oficial</label></br></br>
+       <input  nome="foto"
+        placeholder="Indique imagem para divulgação"
+        type="file"></p>
+
+      <p><label for="">Data </label>
+      <input type="date" name="nome" id="data"></p>
+       <p><label for="">Descrição </label> </p>
+       <textarea name="comentario" rows"15" cols"125"></textarea>
+       <p><button name="enviar" type="submit">Criar</button></p>
+     
+
+	</div>
+	<div id="sidebar">
+			<aside>
+				<ul>
+				<center>Eventos
+				<a href="http://cssday.io" target="_blank">
+				 <img src="http://www.divyweb.com/site/uploads/2015/12/cssdayio-logo-620x269.png"
+				width="130" align="_center" title="click para saber mais">
+				</a><br/>
+
+
+				<a href="http://javaman.io" target="_blank">
+				<img src="http://harrisgray.files.wordpress.com/2013/09/java-man-tour-banner1.png"
+				width="130" align="_center" title="click para saber mais">
+				</a><br/>
+
+				<a href="http://developer.io" target="_blank">
+				<img src="http://www.mattosdesign.com/wallpapers/imagens/wallpaper_developer_blue.jpg"
+				width="130" align="_center" title="click para saber mais">
+				</a><br/></center>
+
+				</ul>
+			</aside>
+		</div>
+		<br><br><br><br><br><br>
+		<footer>
+
+			<a href="#">Fale Conosco</a>
+		</footer>
+	</div>
+</body>
+</html>
